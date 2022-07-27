69a20a6511c3daed576ecba4fbe995a6c2a05931
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-07-27 05:09:57 -0700

    adds initial about me section

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div id="AboutMe" className="container">
+      <div className="row">
+        <h1 className="display-5 fw-bold fontScript-3 text-primary">
+          About Me:
+        </h1>
+        <p>
+          I'm a Full-Stack Developer with a background in Chemical Engineering.
+          I have always been interested in technology, and decided to leverage
+          my engineering background in a new way to complete a Full-Stack Web
+          Development Boot Camp through the University of Washington.
+        </p>
+        <p>
+          When I am not coding, you can catch me camping, hiking, or cycling.
+        </p>
+      </div>
+    </div>
+  );
+}
